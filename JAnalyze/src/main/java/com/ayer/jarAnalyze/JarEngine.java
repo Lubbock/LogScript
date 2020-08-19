76d8d0833d3fbf5d3bed8024ebaafcad11e37ed5
guoxj <Ayer1994@outlook.com>
2020-08-19 09:52:37 +0800

    doc: jar分析原理替换
    jar分析原理替换

--- JAnalyze/src/main/java/com/ayer/jarAnalyze/JarEngine.java
+++ JAnalyze/src/main/java/com/ayer/jarAnalyze/JarEngine.java
@@ -3,13 +3,14 @@ package com.ayer.jarAnalyze;
 import com.ayer.jarAnalyze.analyze.AnalyzePlugin01;
 import com.ayer.jarAnalyze.model.AnalyzeMeta;
 import com.google.common.collect.HashMultimap;
+import lombok.NonNull;
 
 import java.util.Map;
 
 public class JarEngine {
     static AnalyzePlugin analyzePlugin = new AnalyzePlugin01();
 
-    public static void invoke(String fp) {
+    public static void invoke(@NonNull String fp) {
         AnalyzeMeta analyzeMeta = analyzePlugin.analyzeJar(fp);
         HashMultimap<String, String> classMetas = analyzeMeta.getClassMetas();
         for (Map.Entry<String, String> entry : classMetas.entries()) {
--- JAnalyze/src/main/java/com/ayer/jarAnalyze/JavaAnalyze.java
+++ JAnalyze/src/main/java/com/ayer/jarAnalyze/JavaAnalyze.java
@@ -2,6 +2,6 @@ package com.ayer.jarAnalyze;
 
 public class JavaAnalyze {
     public static void main(String[] args) throws Exception {
-        JarEngine.invoke("E:\\titan-sdk-1.0.0-SNAPSHOT.jar");
+        JarEngine.invoke("E:\\temp\\isc\\isc_sso_agent-1.0.0.jar");
     }
 }
